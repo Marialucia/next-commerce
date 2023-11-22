638896dd1af595f5818fae5e8643cc6a1e7a9132
maria.figueiredo <maria.figueiredo@edu.pmro.rj.gov.br>
2023-11-22 14:08:07 -0300

    atualiza 22111407

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,11 @@
 
 export default function Home() {
   return (
-    <main>
+    <div className="max-w-7xl mx-auto pt-8 px-8 xl:px-0">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 xl:gap-6">
+        
+      </div>
      <h1>Next E-commerce 13</h1>
-    </main>
+    </div>
   )
 }
